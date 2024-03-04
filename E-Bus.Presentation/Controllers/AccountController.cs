b0570f7d6c6ae39b7dffc5ece047c34b95d059f5
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-03-04 12:53:23 +0200

    Adding Login and Logout

--- E-Bus.Presentation/Controllers/AccountController.cs
+++ E-Bus.Presentation/Controllers/AccountController.cs
@@ -60,7 +60,7 @@ namespace E_Bus.Presentation.Controllers
                     return View(registerDTO);
                 }
 
-                return RedirectToAction(actionName: "Index", controllerName: "Home");
+                return RedirectToAction(actionName: "Login", controllerName: "Account");
             }
             else
             {
@@ -73,6 +73,44 @@ namespace E_Bus.Presentation.Controllers
         public async Task<IActionResult> IsValidNationalID(string nationalID)
         {
             return Json(await _userManager.FindByNameAsync(nationalID) is null);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage.ToString());
+                var errorsString = string.Join("\n-", errors);
+                ViewBag.Errors = errorsString;
+                return View(loginDTO);
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(userName:loginDTO.NationalID, password:loginDTO.Password,
+                isPersistent:false, lockoutOnFailure:false);
+
+            if(signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            } else
+            {
+                ViewBag.Errors = "Check Credentials Again.";
+                return View(loginDTO);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
