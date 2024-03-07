aaeb128e3a6a0183ac0ed3d79f8bf3e5c9f572e3
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-03-07 19:25:14 +0200

    Adding Home/Index page

--- E-Bus.Presentation/Controllers/HomeController.cs
+++ E-Bus.Presentation/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Bus.Presentation.Controllers
 {
     [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return Content("OK");
+            return View();
         }
+
+
     }
 }
